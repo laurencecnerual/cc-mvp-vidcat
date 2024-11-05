ec2a2fd068fe4ed4b2914a5a860ef06c552f3240
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-05 15:34:06 +0900

    Feat: Adds authentication related functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,6 +40,114 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/signup", async (req, res) => {
+  const { username, password, firstname, lastname } = req.body;
+  const userFound = await getChatUserByUsername(username);
+
+  if (!userFound) {
+    const saltedHash = await hashPassword(password);
+
+    let newChatUser = {
+      username: username,
+      salted_hash: saltedHash,
+      firstname: firstname,
+      lastname: lastname
+    };
+
+    const userCreated = await addUser(newChatUser);
+    delete userCreated[0].salted_hash;
+
+    res.status(201).json(userCreated[0]);
+  } else {
+    res.status(400).send("User Already Exists");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await getChatUserByUsername(username);
+
+  if (user) {
+    const saltedHash = user.salted_hash;
+    const authenicationResult = await verifyPassword(password, saltedHash); //Checks that password is OK
+
+    if (authenicationResult) {
+      req.session.username = user.username; //Gives the user a session because password was OK
+      const lastLoginUpdateResult = await updateLastLogin(user.id, new Date()); //Updates last_login in the db and returns the updated user
+      delete lastLoginUpdateResult[0].salted_hash;
+
+      if (lastLoginUpdateResult[0]) {
+        res.status(200).json({
+          authenticationSuccessful: authenicationResult,
+          gamer: lastLoginUpdateResult[0],
+        });
+      } else {
+        res.status(500).send("Could Not Log In");
+      }
+    } else {
+      res.status(401).json({ authenticationSuccessful: authenicationResult });
+    }
+  } else {
+    res.status(404).send("User Not Found");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Could Not Log Out");
+    }
+    res.clearCookie("connect.sid");
+    res.status(200).send("Log Out Successful");
+  });
+});
+
+//for new user creation
+async function hashPassword(plainTextPassword) {
+  const saltRounds = 10; //the higher the more secure but more time-consuming
+  try {
+    const hash = await bcrypt.hash(plainTextPassword, saltRounds); //applies salt and hashes the password
+    return hash;
+  } catch (err) {
+    console.error("Hashing error:", err);
+  }
+}
+
+//for existing user login
+async function verifyPassword(plainTextPassword, hashedPasswordFromDB) {
+  try {
+    const match = await bcrypt.compare(plainTextPassword, hashedPasswordFromDB); //the salt can be implicitly extracted from the hashed password
+    return match;
+  } catch (err) {
+    console.error("Verification error:", err);
+  }
+}
+
+const GAMER_TABLE = "gamer";
+
+function getChatUserByUsername(username) {
+  return knex
+    .select("*")
+    .from(GAMER_TABLE)
+    .where({ username: username })
+    .first();
+}
+
+function updateLastLogin(id, lastLogin) {
+  return knex(GAMER_TABLE)
+    .returning("*")
+    .where({ id: id })
+    .update({ last_login: lastLogin });
+}
+
+//returns an array of objects, even if just one row being added
+function addUser(newUserObject) {
+  return knex
+    .returning("*")
+    .insert(newUserObject)
+    .into(GAMER_TABLE);
+}
+
 const server = app.listen(PORT, () => {
   console.log(`Express server is up and running on ${PORT}`);
 });

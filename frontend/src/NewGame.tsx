0b13e268057406f984133f5587dd7c2a401635fe
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-07 14:02:46 +0900

    Feat: Fetches user consoles

--- frontend/src/NewGame.tsx
+++ frontend/src/NewGame.tsx
@@ -16,11 +16,21 @@ type Game = {
   background_image_link: string
 }
 
+type UserConsole = {
+  id: number,
+  gamer_id: number,
+  console_id: number,
+  is_owned: boolean,
+  is_favorite: boolean
+}
+
 export default function NewGame({gamer, setAction}: NewGameProps) {
   const [gameList, setGameList] = useState<Game[]>([]);
+  const [userConsoleList, setUserConsoleList] = useState<UserConsole[]>([]);
 
   useEffect(() => {
     handleFetchGames();
+    handleFetchUserConsoles(gamer?.id)
   }, []);
 
   async function handleFetchGames() {
@@ -35,6 +45,41 @@ export default function NewGame({gamer, setAction}: NewGameProps) {
       alert("There was an error loading the games list");
     }
   }
+
+  async function handleFetchUserConsoles(gamerID: number | undefined) {
+    const response = await fetch(apiUrl + `/gamer/${gamerID}/userconsole`, {
+      credentials: "include"
+    });
+
+    if (response.status === 200) {
+      const userConsoleArray = await response.json();
+      setUserConsoleList(userConsoleArray);
+      console.log(userConsoleArray)
+    } else {
+      alert("There was an error loading the user's consoles");
+    }
+  }
+
+  // async function handleAddGame(gamerID: number | undefined, consoleName: string, isOwned: boolean, isFavorite: boolean) {
+  //   const consoleID = (consoleList.find((console) => console.name === consoleName))?.id
+
+  //   const response = await fetch(apiUrl + `/gamer/${gamerID}/userconsole`, {
+  //     method: "POST",
+  //     credentials: "include",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({consoleID: consoleID, isOwned: isOwned, isFavorite: isFavorite})
+  //   });
+
+  //   if (response.status === 200) {
+  //     const payload = await response.json();
+  //     console.log("The following console has been added", payload);
+  //     setAction("PROFILE");
+  //   } else {
+  //     alert("There was an error adding your console");
+  //   }
+  // }
 
   return (
     <>

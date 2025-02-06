be6b938459d84260c2b141edbe59080fbc0f4273
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-06 18:57:48 +0900

    Fix: Further adjusts cookie domain

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -39,7 +39,7 @@ app.use(
       secure: process.env.NODE_ENV_TYPE === "production", 
       sameSite: process.env.NODE_ENV_TYPE === "production" ? "None" : "Lax",
       maxAge: 1000 * 60 * 60 * 24,
-      domain: "vidcat.onrender.com"
+      domain: process.env.NODE_ENV_TYPE === "production" ? "vidcat-backend.onrender.com" : undefined
     },
   })
 );

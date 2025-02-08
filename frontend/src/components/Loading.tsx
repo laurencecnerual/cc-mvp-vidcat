a356cceff57c16ee3ca2dc01eda035a9ddee17e0
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-08 19:50:38 +0900

    Feat: Adds & styles pacman spinner for loading

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,3 +1,20 @@
+import { PacmanLoader } from 'react-spinners';
+
 export default function Loading() {
-  return <h1 className="loading">Loading...</h1>
+  return (
+    <>
+      <div className="loading">
+        <h1>Loading...</h1>
+        <PacmanLoader
+          color="#003366"
+          cssOverride={{
+            marginRight: 120
+          }}
+          margin={2}
+          size={65}
+          speedMultiplier={1}
+        />
+      </div>
+    </>
+  )
 }

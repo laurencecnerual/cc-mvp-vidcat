6b1dc08917d625799ea46aaa2067a63973c7ebc3
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-10 12:10:10 +0900

    Refactor: Moves handleLogout from to GamerContext
    Was part of Header

--- frontend/src/GamerContext.tsx
+++ frontend/src/GamerContext.tsx
@@ -1,17 +1,37 @@
 import { createContext, useContext, useState, useEffect } from 'react';
+import { showToast } from "./ToastHelper.ts";
+const apiUrl: string = import.meta.env.VITE_API_URL;
 
 export const GamerContext = createContext<{
   gamer: Gamer | null;
   setGamer: React.Dispatch<React.SetStateAction<Gamer | null>>;
+  handleLogout: () => void;
 }>({
   gamer: null,
   setGamer: () => {},
+  handleLogout: () => {}
 });
 
 export const useGamer = () => useContext(GamerContext);
 
 export const GamerProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   const [gamer, setGamer] = useState<Gamer | null>(null);
+
+  async function handleLogout() {
+    const response = await fetch(apiUrl + "/logout", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+
+    if (response.status === 200) {
+      setGamer(null);
+    } else {
+      showToast("error", "There was an error logging out");
+    }
+  }
 
   useEffect(() => {
     const savedGamer = localStorage.getItem('gamer');
@@ -31,7 +51,7 @@ export const GamerProvider: React.FC<React.PropsWithChildren<{}>> = ({ children 
   }, [gamer]);
 
   return (
-    <GamerContext.Provider value={{ gamer, setGamer }}>
+    <GamerContext.Provider value={{ gamer, setGamer, handleLogout }}>
       {children}
     </GamerContext.Provider>
   );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,8 @@
-const apiUrl: string = import.meta.env.VITE_API_URL;
 import { useGamer } from "../GamerContext.tsx";
-import { useNavigate, Link, NavLink } from "react-router-dom";
-import { showToast } from "../ToastHelper.ts";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
-  const {gamer, setGamer} = useGamer();
-  const navigate = useNavigate();
-
-  async function handleLogout() {
-    const response = await fetch(apiUrl + "/logout", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
-
-    if (response.status === 200) {
-      setGamer(null);
-      navigate("/login");
-    } else {
-      showToast("error", "There was an error logging out");
-    }
-  }
+  const {gamer, handleLogout} = useGamer();
 
   return (
     <>

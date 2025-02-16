12ea4bad4133b56e38d845c37145814fb10791c6
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-16 17:40:25 +0900

    Feat: Expands and increases seed data

--- backend/db/seeds/001-gamer.js
+++ backend/db/seeds/001-gamer.js
@@ -9,10 +9,11 @@ exports.seed = async function(knex) {
   await knex('gamer').del()
   await knex('gamer').insert([
     {
-      username: 'eyeofkira',
+      username: 'laurencecnerual',
       firstname: 'Laurence',
       lastname: 'Agina',
-      salted_hash: await bcrypt.hash("yoloswag", 10),
+      salted_hash: await bcrypt.hash("yolo", 10),
+      profile_picture: 'https://media.licdn.com/dms/image/v2/D5603AQEzuhM0k7sO5Q/profile-displayphoto-shrink_800_800/B56ZPfWysSH0Ac-/0/1734619114731?e=1745452800&v=beta&t=53nOlkdfPWqLAiFbY4nACPd476ZVSJGSW_hcuvHV_Zk'
     },
   ]);
 };

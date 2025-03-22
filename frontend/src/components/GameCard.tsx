d0d3c99f636a1a7342e866b11e16389ebaa72311
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-03-22 11:13:32 +0900

    Feat: Adds recommendation button to game cards

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import { useGamer } from "../GamerContext.tsx";
 import { useNavigate } from "react-router-dom";
 import { showToast } from "../ToastHelper.ts";
 import Icon from '@mdi/react';
-import { mdiPencilOutline, mdiTrashCanOutline  } from '@mdi/js';
+import { mdiPencilOutline, mdiTrashCanOutline, mdiHeartSearch } from '@mdi/js';
 import ConfirmationModal from "./ConfirmationModal.tsx";
 const apiUrl: string = import.meta.env.VITE_API_URL;
 
@@ -53,11 +53,16 @@ export default function GameCard ({userGame, setRefresh}: GameCardProps) {
     });
   }
 
+  async function getRecommendation() {
+    alert("Implement me");
+  }
+
   return (
     <div className={ gamer?.id === userGame.gamer_id ? "own-card card" : "card" }>
       <div className="card-header">
         { gamer?.id === userGame.gamer_id && <div className="buttons">
           <button type="button" className="edit" onClick={handleEditGame}><Icon path={mdiPencilOutline} size={0.8} /></button>
+          <button type="button" className="recommend" onClick={getRecommendation}><Icon path={mdiHeartSearch} size={0.8} /></button>
           <button type="button" className="delete" onClick={openModal}><Icon path={mdiTrashCanOutline} size={0.8} /></button>
         </div> }
         <div className="game-name">{userGame?.name}</div>

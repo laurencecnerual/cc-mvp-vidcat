abd97101ab0b5a8679f107ac92b4e00f7d737afd
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-04-06 17:05:20 +0900

    Feat: Makes legend icons clickable

--- frontend/src/components/IconLegend.tsx
+++ frontend/src/components/IconLegend.tsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+
 type IconLegendProps = {
   isGameLegend: boolean
 }
 
 export default function IconLegend({isGameLegend}: IconLegendProps) {
+  const [filterOwned, setFilterOwned] = useState(false);
+  const [filterWanted, setFilterWanted] = useState(false);
+  const [filterLoved, setFilterLoved] = useState(false);
+  const [filterBeaten, setFilterBeaten] = useState(false);
+  const [filterWIP, setFilterWIP] = useState(false);
+
+  function handleToggle(state: boolean, setState: Function) {
+    setState(!state);
+  }
+
+  function getLegendIconClassName(state: boolean): string {
+    return state ? "legend-icon active-icon" : "legend-icon";
+  }
+
   return (
     <>
       <div className="legend-container">
         <div className="legend">
-          <div className="owned-icon"><span className="emoji">💸</span> Own it</div>
-          <div className="wanted-icon"><span className="emoji">🙏</span> Want it</div>
-          <div className="favorite-icon"><span className="emoji">❤️</span> Love it</div>
-          { isGameLegend && <div className="completed-icon"><span className="emoji">💯</span> Beat it</div> }
-          { isGameLegend && <div className="in-progress-icon"><span className="emoji">⏳</span> WIP</div> }
+          <div className={getLegendIconClassName(filterOwned)} onClick={() => handleToggle(filterOwned, setFilterOwned)}><span className="emoji">💸</span> Own it</div>
+          <div className={getLegendIconClassName(filterWanted)} onClick={() => handleToggle(filterWanted, setFilterWanted)}><span className="emoji">🙏</span> Want it</div>
+          <div className={getLegendIconClassName(filterLoved)} onClick={() => handleToggle(filterLoved, setFilterLoved)}><span className="emoji">❤️</span> Love it</div>
+          { isGameLegend && <div className={getLegendIconClassName(filterBeaten)} onClick={() => handleToggle(filterBeaten, setFilterBeaten)}><span className="emoji">💯</span> Beat it</div> }
+          { isGameLegend && <div className={getLegendIconClassName(filterWIP)} onClick={() => handleToggle(filterWIP, setFilterWIP)}><span className="emoji">⏳</span> WIP</div> }
         </div>
       </div>
     </>
